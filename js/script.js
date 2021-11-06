007b585601842866e931cf1088834bb286bbe175
nicolebaggerly <nkbaggerly@gmail.com>
2021-11-06 15:03:01 -0700

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,78 @@
+// invite button
+const addGuestButton = document.querySelector(".invite");
+// label for the invite button
+const guestInputLabel = document.querySelector(".add-guest label");
+// text input box
+const guestInput = document.querySelector(".add-guest input");
+// unordered list (not yet visible)
+const guestList = document.querySelector(".guest-list");
+// span class for number of guests attending
+const guestCount = document.querySelector(".attendance");
+// alert when guest list is full (not yet visible)
+const guestFull = document.querySelector(".alert");
+
+const assignButton = document.querySelector(".assign");
+const assignedItems = document.querySelector(".assigned-items");
+
+addGuestButton.addEventListener("click", function () {
+  const guest = guestInput.value;
+  //console.log(guest);
+  if (guest !== "") {
+    addToList(guest);
+    clearInput();
+    updateGuestCount();
+  }
+});
+
+const clearInput = function () {
+  guestInput.value = "";
+};
+
+const addToList = function (guest) {
+  const listItem = document.createElement("li");
+  listItem.innerText = guest;
+  guestList.append(listItem);
+};
+
+const updateGuestCount = function () {
+  const guests = document.querySelectorAll(".guest-list li");
+  guestCount.innerText = guests.length;
+  if (guests.length === 8) {
+    addGuestButton.classList.add("hide");
+    guestInput.classList.add("hide");
+    guestInputLabel.classList.add("hide");
+    guestFull.classList.remove("hide");
+  }
+};
+
+const assignItems = function () {
+  const potluckItems = [
+    "nachos",
+    "sliders",
+    "guacamole",
+    "charcuterie",
+    "sangria",
+    "fried rice",
+    "cookies",
+    "chips",
+    "chicken wings",
+    "pizza",
+    "fruit",
+    "potato salad"
+  ];
+  const allGuests = document.querySelectorAll(".guest-list li"); //I had used guestList as the value instead of using querySelector
+
+  for (let guest of allGuests) {
+    let randomPotluckIndex = Math.floor(Math.random() * potluckItems.length);
+    let randomPotluckItem = potluckItems[randomPotluckIndex]; //This step really confused me. For some reason  didn't think I could pull the index position just by using the variable name.
+    let listItem = document.createElement("li");
+    listItem.innerText = `${guest.innerText} is bringing ${randomPotluckItem}.`;
+    assignedItems.append(listItem);
+    potluckItems.splice(randomPotluckIndex, 1);
+  }
+};
+
+assignButton.addEventListener("click", function () {
+  assignItems();
+  assignButton.disabled = true;
+});
